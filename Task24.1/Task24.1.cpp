86829e810b3bf18ddac302a796ebd82f1eb4124b
DmityFilchev <filchev_dmitry@mail.ru>
2022-09-26 16:12:11 +0300

    Добавьте файлы проекта.

---
+++ Task24.1/Task24.1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include<ctime>
+
+int main()
+{
+	std::time_t t = time(nullptr);
+	int f = 0;
+	std::cin >> f;
+	std::time_t d = time(NULL);
+
+	double tin = difftime(d, t);
+	std::time_t c = d + (std::time_t)tin;
+
+	std::tm* local = std::localtime(&c);
+	std::cout <<std::asctime(local) << std::endl;
+	
+}
